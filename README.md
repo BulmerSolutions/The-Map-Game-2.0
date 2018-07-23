73b92470122c20541fafae9930f61c2cc4f0b528
Dylan Bulmer <dylanmbulmer@gmail.com>
2018-07-23 15:11:05 -0400

    started to create the game page. added socket io

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-﻿# TheMapGame
+﻿# The Map Game 2.0
 
+The Map Game was never finished in the last go around, but this time we are writting it in Node.JS and Socket.IO to make development much easier.
 
+**Current Features:**
+
+* Nothing
+
+**Future Features:**
+
+* In-game chat
+* Responsive map with ability to draw on it live
---
+++ routes/game.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET page to enter game pin */
+router.get('/', function (req, res) {
+    res.render('gameLogin', { title: 'The Map Game' });
+});
+
+/* GET game and chack session for game session code */
+router.get('/:pin', function (req, res) {
+    res.send('Game pin is: ' + req.params.pin);
+
+    // Create Socket IO connections here
+    let io = req.app.get('socketio');
+});
+
+module.exports = router;
